cc3c58a94a8af825dfcabacb3a6b62fca280ea6c
cinexfilter <106576204+cinexfilter@users.noreply.github.com>
2023-07-12 08:49:56 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -75,10 +75,6 @@ async def next_page(bot, query):
                 InlineKeyboardButton(
                     text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
-                InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
-                    callback_data=f'files_#{file.file_id}',
-                ),
             ]
             for file in files
         ]
